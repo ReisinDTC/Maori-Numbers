cc02d1c094a735228497cfc9dc7dc26967ded1b2
ReidisinDTC <124422015+ReisinDTC@users.noreply.github.com>
2023-05-05 15:07:09 +1200

    update

--- 01_Welcome_user_v4.py
+++ 01_Welcome_user_v4.py
@@ -10,15 +10,17 @@ while show_instructions != "x":
     show_instructions = input("Would you like the instructions of this quiz?: ")
 
     #If they say yes, output 'Program Continues'
-    if show_instructions == "yes" or show_instructions == "y":
+    if show_instructions == "yes" or show_instructions == "Yes" or show_instructions == "y":
         print("Program continues")
 
     #If they say no, output 'Display instructions'
-    elif show_instructions == "no" or show_instructions == "n":
-        print("Display instructions")
+    elif show_instructions == "no" or show_instructions == "No" or show_instructions == "n":
+        print("Program continues")
 
     #Otherwise - show error
     else:
         print("Please answer 'yes' or 'no'")
 
     print(f"You entered '{show_instructions}'")
+
+
